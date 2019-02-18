b13163f37e0f32f933f913399d163aaf048c6a17
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-18 15:01:09 +0000

    MVP: Added props validator for CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Comment from './Comment';
 
 const CommentSection = ({ comments }) => (
   <div>
     {comments.map(({ username, text }) => (
-      <Comment username={username} text={text} />
+      <Comment key={`${username}-${text}`} username={username} text={text} />
     ))}
+    <div>
+      <input placeholder="Add comment..." />
+      <p>...</p>
+    </div>
   </div>
 );
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    text: PropTypes.string,
+  })).isRequired,
+};
 
 export default CommentSection;
