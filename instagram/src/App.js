40aa0a9f4752b1c02b98b1cfff0505a6c5cb8f0b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-18 12:57:02 +0000

    MVP: Added assets and created SearchBar component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+       <SearchBar />
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CameraIg from '../../assets/camera-ig.png';
+import LogoIg from '../../assets/insta-logo.svg';
+import Compass from '../../assets/compass.png';
+import Heart from '../../assets/heart.png';
+import User from '../../assets/user.png';
+import './SearchBar.css';
+
+const SearchBar = () => (
+  <div className="search-bar">
+    <div className="logo">
+      <img className="camera" src={CameraIg} alt="camera-ig" />
+      <img className="insta-logo" src={LogoIg} alt="logo-ig" />
+    </div>
+    <input className="search" placeholder="Search..." />
+    <div className="social-icons">
+      <img className="icon compass" src={Compass} alt="compass" />
+      <img className="icon heart" src={Heart} alt="heart" />
+      <img className="icon user" src={User} alt="user" />
+    </div>
+  </div>
+);
+
+export default SearchBar;
