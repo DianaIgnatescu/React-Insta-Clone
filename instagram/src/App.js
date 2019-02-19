156577b2e0312d6466dc378374d33aaf22d958c9
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-19 12:33:21 +0000

    MVP: Used lifecycle method to set the data to the component's state

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -6,11 +6,25 @@ import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [...dummyData],
+      // comments: [{ postId: 1, username: 'biancasaurus', text: 'hello'}, { postId: 1, username: 'theotherguy', text: 'hello'},
+      // { postId: 2, username: 'biancasaurus', text: 'hello'}],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ posts: [...dummyData] });
+  }
+
   render() {
+    const { posts } = this.state;
     return (
       <div className="App">
         <SearchBar />
-        {dummyData.map(post => (
+        {posts.map(post => (
           <PostContainer
             key={post.timestamp}
             comments={post.comments}
