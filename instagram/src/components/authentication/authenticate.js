c3c47c3a3300d6b39b066fdd75874aae747fec8c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-20 14:38:59 +0000

    MVP: Completed functionality to show Login page when loggedIn is false

--- instagram/src/components/authentication/authenticate.js
+++ instagram/src/components/authentication/authenticate.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
 const authenticate = App => Login => class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  componentDidMount() {
+    const username = localStorage.getItem('username');
+    if (username) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  }
 
   render() {
+    const { loggedIn } = this.state;
+    if (loggedIn) {
+      return <App />;
+    }
     return (
-      <App />
+      <Login />
     );
   }
 };
