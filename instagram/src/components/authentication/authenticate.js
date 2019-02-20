6a0eb342764d8ba574b4db523e8275cbd9666dcf
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-20 13:43:01 +0000

    MVP: Created HOC

---
+++ instagram/src/components/authentication/authenticate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const authenticate = App => class extends React.Component {
+
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+export default authenticate;
