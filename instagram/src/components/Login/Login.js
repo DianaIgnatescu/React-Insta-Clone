1fed451700756feaa3743eb4ac5ae409afb68789
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-20 14:01:28 +0000

    MVP: Set username on localStorage

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+const login = (username, password) => {
+  if (username === 'holly' && password === '1234') {
+    localStorage.setItem('username', username);
+    return true;
+  }
+  return false;
+};
+
+const handleSubmit = (event) => {
+  const username = event.target.querySelector('input[name="username"]').value;
+  const password = event.target.querySelector('input[name="password"]').value;
+  const successfulLogin = login(username, password);
+};
+
 const Login = () => (
   <div className="login">
-    <form>
-      <input placeholder="username" />
-      <input placeholder="password" />
+    <form onSubmit={handleSubmit}>
+      <input name="username" placeholder="username" />
+      <input name="password" type="password" placeholder="password" />
       <button type="submit">LOGIN</button>
     </form>
   </div>
